f43da1ea7f0d5432d78925bb488753586b435700
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2024-02-19 13:57:42 -0500

    defined columns in Tag model file

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,16 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // Define what's inside the Tag table
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
